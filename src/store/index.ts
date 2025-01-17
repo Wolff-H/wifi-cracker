940c2092f0507eb91fc7d47d54960593668fa386
Wolff <hutingrui01@yeah.net>
2025-01-17 13:29:45 +0800

    feat: home header

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,12 +11,14 @@ import { defineStore } from "pinia"
 const useStore = defineStore('/', {
     state: (): _S => ({
         initialized_time: dayjs().unix(),
+        navigation_at: 'tasks',
     }),
 })
 
 type _S =
 {
     initialized_time: number
+    navigation_at: 'tasks' | 'scan' | 'device'
 }
 
 // export type Store = ReturnType<typeof useStore> // 不需要手写了，因为由 store.auto-imports.ts 自动编译了
--- src/ui/view-home/index.vue
+++ src/ui/view-home/index.vue
@@ -2,15 +2,24 @@
 #view-home
     #header
         #navigation
-            .navigation-tab.tasks
+            .navigation-tab.tasks(
+                :class="{ 'o-active': store.navigation_at === 'tasks' }"
+                @click="store.navigation_at = 'tasks'"
+            )
                 IconListTop.icon(size="16px")
                 .title
                     |任务
-            .navigation-tab.scan
+            .navigation-tab.scan(
+                :class="{ 'o-active': store.navigation_at === 'scan' }"
+                @click="store.navigation_at = 'scan'"
+            )
                 IconRadarTwo.icon(size="16px")
                 .title
                     |扫描
-            .navigation-tab.device
+            .navigation-tab.device(
+                :class="{ 'o-active': store.navigation_at === 'device' }"
+                @click="store.navigation_at = 'device'"
+            )
                 IconSignalOne.icon(size="16px")
                 .title
                     |设备
@@ -44,13 +53,12 @@ $root = '#view-home'
         height 41px
         width 100%
         border-bottom 1px solid $black15
-        background-color $black03
+        background-color $black02
         #navigation
             display flex
             height 100%
             .navigation-tab
                 display flex
-                // flex-direction column
                 height 100%
                 width 80px
                 align-items center
@@ -59,7 +67,9 @@ $root = '#view-home'
                 .icon
                     // 
                 .title
-                    margin-left 8px
-                    font-size 16px
-                    // color $black40
+                    margin-left 6px
+                    font-size 14px
+                &.o-active
+                    color $primary
+
 </style>
