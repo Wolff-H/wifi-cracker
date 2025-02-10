8be31dafb923f0251a0d850a524e9708c24e6854
Wolff <hutingrui01@yeah.net>
2025-02-10 21:12:44 +0800

    fix: shell调用语言和隐藏窗口

--- src-tauri/src/utils/run_command.rs
+++ src-tauri/src/utils/run_command.rs
@@ -1,24 +1,24 @@
+use std::os::windows::process::CommandExt;
 use std::process::Command;
 use std::io::{BufRead, BufReader};
-use shlex::Shlex;
-
+// use shlex::Shlex;
 
 
 /**
  * 执行命令行指令，返回纯文本的执行结果（自动依系统语言）
  */
 pub fn run_command(command_line: &str) -> Result<String, std::io::Error> {
-    // let mut command = Command::new("powershell");
-    // command
-    //     .arg("-Command")
-    //     .arg(format!("$OutputEncoding = [console]::OutputEncoding = [System.Text.Encoding]::UTF8; Set-Culture en-US; {}", command_line));
+    let mut command = Command::new("powershell");
+    command
+        .arg("-Command")
+        .arg(format!("$OutputEncoding = [console]::OutputEncoding = [System.Text.Encoding]::UTF8; Set-Culture en-US; {}", command_line));
 
-    let mut args = Shlex::new(command_line);
-    let mut command = Command::new(args.next().unwrap());
+    // let mut args = Shlex::new(command_line);
+    // let mut command = Command::new(args.next().unwrap());
 
-    command.args(args);
+    // command.args(args);
 
-    let output = command.output()?;
+    let output = command.creation_flags(0x08000000).output()?;
     let reader = BufReader::new(output.stdout.as_slice());
     let mut result = String::new();
 
